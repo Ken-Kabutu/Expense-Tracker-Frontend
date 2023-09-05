8ab63ed6ab58c6ba38461721bd774a006e9ac720
Ken <kabutukenneth3@gmail.com>
2023-09-05 07:04:41 +0300

    validate login form

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -16,4 +16,23 @@ const UserLogin = () => {
         setFormData({ ...formData, [name]: value });
     };
 
+      // Function to validate the login form
+    const validateForm = () => {
+        const newErrors = {};
+
+        if (!formData.email.trim()) {
+        newErrors.email = 'Email is required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(formData.email)) {
+        newErrors.email = 'Invalid email address';
+        }
+
+        if (!formData.password.trim()) {
+        newErrors.password = 'Password is required';
+        }
+
+        // Set errors and return true if there are no errors
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
 }
