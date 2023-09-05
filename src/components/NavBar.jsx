f1c161ccf9a719022db75617217ca6e660bad480
isaiahsikobe <isaiah.sikobe@student.moringaschool.com>
2023-09-05 12:49:16 -0600

    Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+// Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/expense-entry">Expense Entry</Link>
+        </li>
+        <li>
+          <Link to="/expense-list">Expense List</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/registration">Registration</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
