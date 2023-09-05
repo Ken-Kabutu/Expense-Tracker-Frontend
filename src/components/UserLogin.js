54f22f3feb0504b62a57aff037e44f652b8d4d03
Ken <kabutukenneth3@gmail.com>
2023-09-05 06:59:03 +0300

    set state for form data in userlogin

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -2,5 +2,10 @@ import React, { useState } from 'react';
 
 
 const UserLogin = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
     
+    const [errors, setErrors] = useState({});
 }
