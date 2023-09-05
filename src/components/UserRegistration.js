c3a40bdd1ecb468460f6da997d2e1c8b5e5d5061
Ken <kabutukenneth3@gmail.com>
2023-09-05 06:41:48 +0300

    validate the registration form

--- src/components/UserRegistration.js
+++ src/components/UserRegistration.js
@@ -15,5 +15,26 @@ const UserRegistration = () => {
         setFormData({ ...formData, [name]: value });
     };
 
-    
+
+  // Function to validate the registration form
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!formData.name.trim()) {
+      newErrors.name = 'Name is required';
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = 'Email is required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(formData.email)) {
+      newErrors.email = 'Invalid email address';
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = 'Password is required';
+    } else if (formData.password.length < 6) {
+      newErrors.password = 'Password must be at least 6 characters';
+    }
+    }
+
 }
