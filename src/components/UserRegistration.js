01f6e14630a878de0049e78935ee9f60610215da
Ken <kabutukenneth3@gmail.com>
2023-09-05 06:36:39 +0300

    set state for form data

--- src/components/UserRegistration.js
+++ src/components/UserRegistration.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 
 const UserRegistration = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
     
+    const [errors, setErrors] = useState({});
 }
